4d5f3db40bef8da5ffced1abf24b116bf7e311bd
studylessshape <a664554724@qq.com>
2025-05-27 09:36:55 +0800

    update: update support .net and add wpf projcet

---
+++ Less.Utils.WPF/BindingProxy.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace Less.Utils.WPF
+{
+    /// <summary>
+    /// <seealso href="https://www.cnblogs.com/czwy/p/17638959.html"/>
+    /// </summary>
+    public class BindingProxy : Freezable
+    {
+        protected override Freezable CreateInstanceCore()
+        {
+            return new BindingProxy();
+        }
+
+        public object Data
+        {
+            get { return GetValue(DataProperty); }
+            set { SetValue(DataProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Data.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DataProperty =
+            DependencyProperty.Register("Data", typeof(object), typeof(BindingProxy), new PropertyMetadata(null));
+    }
+}
