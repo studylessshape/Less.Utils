325d5e311a060081316d581da9008d2487f6ccd7
studylessshape <a664554724@qq.com>
2025-05-28 14:12:26 +0800

    feat: add event validation

---
+++ Less.Utils.WPF/EventValidation.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace Less.Utils.WPF
+{
+    public class EventValidation : ValidationRule
+    {
+        public delegate ValidationResult ValidationEvent(object value, CultureInfo cultureInfo);
+        public event ValidationEvent ValidateEvent;
+        public bool DefaultResult { get; set; } = true;
+        public string DefaultErrorContent { get; set; } = "";
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var result = ValidateEvent?.Invoke(value, cultureInfo);
+            if (result == null)
+            {
+                return new ValidationResult(DefaultResult, DefaultErrorContent);
+            }
+            return result;
+        }
+    }
+}
