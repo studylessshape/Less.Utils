325c0033f112a03f8b34cd55db3e70eb4617df6b
studylessshape <a664554724@qq.com>
2025-06-04 10:54:15 +0800

    chore: add EventValidation comment

--- Less.Utils.WPF/EventValidation.cs
+++ Less.Utils.WPF/EventValidation.cs
@@ -3,11 +3,29 @@ using System.Windows.Controls;
 
 namespace Less.Utils.WPF
 {
+    /// <summary>
+    /// Custom validation by using event
+    /// </summary>
     public class EventValidation : ValidationRule
     {
+        /// <summary>
+        /// Call in <see cref="Validate(object, CultureInfo)"/>
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="cultureInfo"></param>
+        /// <returns></returns>
         public delegate ValidationResult ValidationEvent(object value, CultureInfo cultureInfo);
+        /// <summary>
+        /// Call in <see cref="Validate(object, CultureInfo)"/>
+        /// </summary>
         public event ValidationEvent ValidateEvent;
+        /// <summary>
+        /// When result is <see langword="null"/>, the result will use default result
+        /// </summary>
         public bool DefaultResult { get; set; } = true;
+        /// <summary>
+        /// When result is <see langword="null"/>, the error content will use default result
+        /// </summary>
         public string DefaultErrorContent { get; set; } = "";
         public override ValidationResult Validate(object value, CultureInfo cultureInfo)
         {
