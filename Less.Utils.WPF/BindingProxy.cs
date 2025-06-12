003dc90a37efe837d089312f2b524e0c0ea19f48
studylessshape <a664554724@qq.com>
2025-06-12 10:56:49 +0800

    feat: add auto tooltip for `RoutedCommand`

--- Less.Utils.WPF/BindingProxy.cs
+++ Less.Utils.WPF/BindingProxy.cs
@@ -7,18 +7,24 @@ namespace Less.Utils.WPF
     /// </summary>
     public class BindingProxy : Freezable
     {
+        /// <inheritdoc/>
         protected override Freezable CreateInstanceCore()
         {
             return new BindingProxy();
         }
 
+        /// <summary>
+        /// Bind data
+        /// </summary>
         public object Data
         {
             get { return GetValue(DataProperty); }
             set { SetValue(DataProperty, value); }
         }
 
-        // Using a DependencyProperty as the backing store for Data.  This enables animation, styling, binding, etc...
+        /// <summary>
+        /// Using a <see cref="DependencyProperty"/> as the backing store for <see cref="Data"/>.  This enables animation, styling, binding, etc...
+        /// </summary>
         public static readonly DependencyProperty DataProperty =
             DependencyProperty.Register("Data", typeof(object), typeof(BindingProxy), new PropertyMetadata(null));
     }
